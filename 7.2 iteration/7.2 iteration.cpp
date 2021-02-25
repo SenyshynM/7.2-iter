5e7e38eda026b7bebbb74b0d43e481d515e1dc4b
SenyshynM <73256299+SenyshynM@users.noreply.github.com>
2021-02-25 10:24:50 +0200

    first commit

---
+++ 7.2 iteration/7.2 iteration.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <time.h>
+
+using namespace std;
+
+void Create(int** a, const int rowCout, const int colCout, const int Low, const int High);
+void Print(int** a, const int rowCout, const int colCout);
+int Sum(int** a, const int rowCount, const int colCount);
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	int k, n, Low, High;
+	cout << "Low = "; cin >> Low;
+	cout << "High = "; cin >> High;
+	cout << "k = "; cin >> k;
+	cout << "n = "; cin >> n;
+
+	int rowCount = n;
+	int colCount = k;
+
+	int** a = new int* [rowCount];
+	for (int i = 0; i < rowCount; i++)
+		a[i] = new int[colCount];
+
+	Create(a, rowCount, colCount, Low, High);
+	Print(a, rowCount, colCount);
+
+	int S = Sum(a, rowCount, colCount);
+	cout << "max sum = " << S << endl;
+
+	for (int i = 0; i < rowCount; i++)
+		delete[] a[i];
+	delete[] a;
+	return 0;
+}
+
+void Create(int** a, const int rowCount, const int colCount, const int Low, const int High)
+{
+	for (int i = 0; i < rowCount; i++)
+		for (int j = 0; j < colCount; j++)
+			a[i][j] = Low + rand() % (High - Low + 1);
+}
+
+void Print(int** a, const int rowCount, const int colCount)
+{
+	cout << endl;
+	for (int i = 0; i < rowCount; i++)
+	{
+		for (int j = 0; j < colCount; j++)
+			cout << setw(4) << a[i][j];
+		cout << endl;
+	}
+	cout << endl;
+}
+
+int Sum(int** a, const int rowCount, const int colCount)
+{
+	int s = 0;
+	for (int j = 1; j < colCount; j++)
+	{
+		if (j % 2 != 0)
+		{
+			int max = a[0][j];
+			for (int i = 0; i < rowCount; i++)
+			{
+				if (a[i][j] > max)
+					max = a[i][j];
+			}
+			
+			s += max;
+		}
+	}
+	return s;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../7.2 iteration/7.2 iteration.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		TEST_METHOD(TestMethod1)
+		{
+			int q[3][3] = { {1,1,1},{2,2,2},{3,3,3} };
+			int s;
+			int* q1[3] = { q[0],q[1],q[2] };
+
+			s = Sum(q1, 3, 3);
+
+			Assert::AreEqual(3, s);
+		}
+	};
+}
